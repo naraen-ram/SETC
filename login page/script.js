505574785e2de51e0d814b77989019e550681384
Jeeva8525 <jeevananthan8507@gmail.com>
2025-08-11 22:31:03 +0530

    completed the functionality of remember me checkbox

--- login page/script.js
+++ login page/script.js
@@ -5,6 +5,16 @@ function sleep(ms) {
     })
 }
 
+window.onload = function () {
+    //console.log("Page loaded, checking for saved credentials...");
+    if (localStorage.getItem("remember") === "true") {
+        //console.log("Remember me is checked, loading saved credentials.");
+        document.getElementById("username").value = localStorage.getItem("username");
+        document.getElementById("password").value = localStorage.getItem("password");
+        document.getElementById("rememberMe").checked = true;
+    }
+};
+
 var invalidEmailOrPass = document.getElementById("invalidEmailOrPass");
 
 let ids = [], passwords = [];
@@ -18,8 +28,8 @@ async function getIdsAndPasswords() {
     let data = datas.userPasswords; /* accessing the userPasswords array from the JSON data */
     ids = data.map(user => user.username)
     passwords = data.map(user => user.password)
-    console.log("IDs:", ids)
-    console.log("Passwords:", passwords)
+    //console.log("IDs:", ids)
+    //console.log("Passwords:", passwords)
 }
 getIdsAndPasswords()  /* The function is called here */
 
@@ -27,34 +37,49 @@ let username = document.getElementById("username")
 let password = document.getElementById("password")
 let loginButton = document.querySelector(".login-button")
 let loginContainer = document.querySelector(".login-container")
+let rememberMe = document.getElementById("rememberMe");
+//console.log("Remember me status:", rememberMe.checked)
 
 document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault()
-    // console.log("Form submission prevented")
+    event.preventDefault()      // console.log("Form submission prevented")
 });
 
 /* Function for the submit button */
-loginButton.addEventListener("click",async function () {
+loginButton.addEventListener("click", async function () {
     let user = username.value
     let pass = password.value
-    if(user==="" || pass ==="" ){
+    if (user === "" || pass === "") {
         return
     }
     for (let x = 0; x < ids.length; x++) {
         if (user === ids[x] && pass === passwords[x]) {
             console.log("Login successful for user:", user)
             await sleep(100);
+            loginButton.style.bottom = "0px";
             invalidEmailOrPass.style.display = "none"
             
-            window.location.href='https://www.youtube.com';
+            if (rememberMe.checked) {       //sets remember password if login is successful
+                localStorage.setItem("username", user);
+                localStorage.setItem("password", pass);
+                localStorage.setItem("remember", true);
+            } else {                        //clears saved credentials if remember me is not checked
+                username.value = ""
+                password.value = ""
+                localStorage.removeItem("username");
+                localStorage.removeItem("password");
+                localStorage.setItem("remember", false);
+            }
+
+            window.location.href = 'https://www.tnstc.in/OTRSOnline/';
             return
 
         }
     }
-    
-    console.log("Login failed. Invalid username or password.")
+
+    //console.log("Login failed. Invalid username or password.")
     await sleep(1); // to overcome "Please select the field error || if you dont understand this, just comment this line and see what happens"
     invalidEmailOrPass.style.display = "block"
+    loginButton.style.bottom = "12px"; /* Added this to adjust the position of the login button as it moves so down so that it touches the margin */
     username.value = ""
     password.value = ""
     username.focus()
