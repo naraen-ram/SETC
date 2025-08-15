df1b64cd806bff5a518214d4447c6389c2ca2781
Vishveshwaran A <vishveshwaran632007@gmail.com>
2025-08-15 12:12:49 +0530

    Added DashBoard

---
+++ dash_board/dash.js
@@ -0,0 +1,13 @@
+
+
+
+let menu=document.querySelector(".menu");
+let button=document.querySelector(".menu-button");
+
+function openmenu()
+{
+   menu.classList.toggle("menu-open");
+   button.classList.toggle("menu-button-open");
+
+
+}
