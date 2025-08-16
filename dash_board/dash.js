0520a373fac342062ea690b3994d1e7a98776a26
J0sh-ini <joshiniyan006@gmail.com>
2025-08-16 20:49:16 +0530

    data changes to dashboard

--- dash_board/dash.js
+++ dash_board/dash.js
@@ -17,34 +17,37 @@ function openmenu()
 let allData = [];
 async function getdata() 
 {   
-    console.log(1);
     
     
-    let jsonFile = await fetch("attendance.json");
+    let jsonFile = await fetch("dummy.json");
     if (!jsonFile.ok) 
     {
         throw new Error("can't pull data");
     }
     allData = await jsonFile.json();
    
-    let count=[0,0,0];
+    let inCount=outCount=lateCount=activeCount=absentCount=0;
     for(let i=0;i<allData.length;i++)
-    {   
-        count[0]++;
-        if(allData[i].outtime!=null)
-            count[1]++;
+    {  
+        if(allData[i].intime!=null)
+        inCount++;
+        if(allData[i].out_time!=null)
+            outCount++;
           let intime = new Date(`1970-01-01T${allData[i].intime}`);
         let cutoff = new Date(`1970-01-01T09:15:00`);
 
         if (intime > cutoff) 
-            count[2]++; 
-
+            lateCount++; 
+        
+        activeCount=inCount-outCount;
     }
+absentCount=allData.length-inCount;
 
-
-    document.querySelector("#in").innerHTML=count[0].toString();
-    document.querySelector("#out").innerHTML=count[1].toString();
-    document.querySelector("#late").innerHTML=count[2].toString();
+    document.querySelector("#in").innerHTML=inCount.toString();
+    document.querySelector("#out").innerHTML=outCount.toString();
+    document.querySelector("#late").innerHTML=lateCount.toString();
+    document.querySelector("#absent").innerHTML=absentCount.toString();
+    document.querySelector("#active").innerHTML=activeCount.toString();
   
     
 }
