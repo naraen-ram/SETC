cb0dec9dd70aa4615067d500b3e7084b58676f23
Jeeva8525 <jeevananthan8507@gmail.com>
2025-08-18 20:18:17 +0530

    updated the user-name in the top right corner of dashboard

--- dash_board/dash.js
+++ dash_board/dash.js
@@ -1,54 +1,43 @@
+const parameters = new URLSearchParams(window.location.search);  //from the url , username is retrieved
+const loginUserName = parameters.get('loginName');
+console.log(loginUserName);
+document.querySelector(".user-name").textContent = loginUserName;
 
-
-
-let menu=document.querySelector(".menu");
-let button=document.querySelector(".menu-button");
-function openmenu()
-{
-   menu.classList.toggle("menu-open");
-   button.classList.toggle("menu-button-open");
-
+let menu = document.querySelector(".menu");
+let button = document.querySelector(".menu-button");
+function openmenu() {
+    menu.classList.toggle("menu-open");
+    button.classList.toggle("menu-button-open");
 }
-
-
-
-
 let allData = [];
-async function getdata() 
-{   
-    
-    
+async function getdata() {
     let jsonFile = await fetch("../database/dummy.json");
-    if (!jsonFile.ok) 
-    {
+    if (!jsonFile.ok) {
         throw new Error("can't pull data");
     }
     allData = await jsonFile.json();
-   
-    let inCount=outCount=lateCount=activeCount=absentCount=0;
-    for(let i=0;i<allData.length;i++)
-    {  
-        if(allData[i].intime!=null)
-        inCount++;
-        if(allData[i].out_time!=null)
+
+    let inCount = outCount = lateCount = activeCount = absentCount = 0;
+    for (let i = 0; i < allData.length; i++) {
+        if (allData[i].intime != null)
+            inCount++;
+        if (allData[i].out_time != null)
             outCount++;
-          let intime = new Date(`1970-01-01T${allData[i].intime}`);
+        let intime = new Date(`1970-01-01T${allData[i].intime}`);
         let cutoff = new Date(`1970-01-01T09:15:00`);
 
-        if (intime > cutoff) 
-            lateCount++; 
-        
-        activeCount=inCount-outCount;
-    }
-absentCount=allData.length-inCount;
+        if (intime > cutoff)
+            lateCount++;
 
-    document.querySelector("#in").innerHTML=inCount.toString();
-//    document.querySelector("#out").innerHTML=outCount.toString();
-    document.querySelector("#late").innerHTML=lateCount.toString();
-    document.querySelector("#absent").innerHTML=absentCount.toString();
+        activeCount = inCount - outCount;
+    }
+    absentCount = allData.length - inCount;
+
+    document.querySelector("#in").innerHTML = inCount.toString();
+    //    document.querySelector("#out").innerHTML=outCount.toString();
+    document.querySelector("#late").innerHTML = lateCount.toString();
+    document.querySelector("#absent").innerHTML = absentCount.toString();
     // document.querySelector("#active").innerHTML=activeCount.toString();
-  
-    
 }
 
 getdata();
