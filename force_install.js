180841eaf9d0132d8a29dfdb331b1543f8e14b05
Lokesh Selvam <lokeshselvam7025@gmail.com>
2026-01-27 18:46:41 +0530

    final

--- force_install.js
+++ force_install.js
@@ -31,4 +31,12 @@ svc.on('uninstall', function(){
   svc.install();
 });
 
-svc.install();foce
+svc.install();
+
+pm2 start bridge.js --name "BiometricBridge"
+
+npm install pm2-windows-startup -g
+
+pm2-startup install
+
+pm2 save
